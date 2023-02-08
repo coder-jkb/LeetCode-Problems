e0bd7b049a011f43420fa5c574d9a2da31c9cd39
Jay Bhanushali <jkb17codes@gmail.com>
2023-02-08 21:13:55 +0530

    Time: 510 ms (56.95%), Space: 15.1 MB (78.83%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -1,10 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         max_inx = 0
-        for inx, jump in enumerate(nums):
+        for inx in range(len(nums)):
             if inx > max_inx:
                 return False
-            max_inx = max(inx+jump, max_inx)
+            
+            max_inx = max(inx+nums[inx], max_inx)
             
         return True
                 
