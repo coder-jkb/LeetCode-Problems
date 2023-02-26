2d392a66aecab4c224750a1b93f21ce2441f462e
Jay Bhanushali <jkb17codes@gmail.com>
2023-02-26 20:10:41 +0530

    Time: 39 ms (45.41%), Space: 13.8 MB (95.91%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,4 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        # nums.sort() # one liner solution in python
+        i=0; left=0; right=len(nums)-1
+        
+        def swap(a,b):
+            nums[a], nums[b] = nums[b], nums[a]
+            
+        while i<=right:
+            if nums[i]==0:
+                swap(i,left)
+                i+=1; left+=1
+                
+            elif nums[i]==1:
+                i+=1
+            
+            else:
+                swap(i,right)
+                right-=1
