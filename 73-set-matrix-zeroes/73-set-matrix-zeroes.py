0f8087ba16d3d2caecc4a2f2204b4df23dbf5a5e
Jay Bhanushali <jkb17codes@gmail.com>
2023-02-26 19:00:21 +0530

    Time: 133 ms (67.26%), Space: 14.8 MB (66.07%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        i=0; j=0
         nrows, ncols = len(matrix), len(matrix[0])
         zeros = dict()
         zeros['rows'] = set()
@@ -12,8 +13,11 @@ class Solution:
                 if matrix[i][j] == 0:
                     zeros['rows'].add(i)
                     zeros['cols'].add(j)
-                    
-        for row in range(nrows):
+            
+        for row in list(zeros['rows']):
             for col in range(ncols):
-                if row in zeros['rows'] or col in zeros['cols']:
-                    matrix[row][col] = 0
+                matrix[row][col] = 0
+                
+        for col in list(zeros['cols']):
+            for row in range(nrows):
+                matrix[row][col] = 0
