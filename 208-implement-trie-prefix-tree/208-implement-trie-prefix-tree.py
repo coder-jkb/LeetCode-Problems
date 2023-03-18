aa43c81d003308662b079258b9c528dc31bb9d96
Jay Bhanushali <jkb17codes@gmail.com>
2023-03-18 17:36:07 +0530

    Time: 178 ms (61.31%), Space: 31.7 MB (52.72%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -1,51 +1,37 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
-        self.end_of_word = False
+        self.chars = dict()
+        self.end = False
         
-
 class Trie:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.root = TrieNode()
-        
 
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
         node = self.root
-        for char in word:
-            if char not in node.children:
-                node.children[char] = TrieNode()
-            node = node.children[char]
-        node.end_of_word = True
-        
-
-    def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
+        for ch in word:
+            if ch not in node.chars:
+                node.chars[ch] = TrieNode()
+            node = node.chars[ch]
+        node.end = True
+                
+            
+    def search(self, word: str) -> bool:        
         node = self.root
-        for char in word:
-            if char not in node.children:
+        for ch in word:
+            if ch not in node.chars:
                 return False
-            node = node.children[char]
-        return node.end_of_word
-        
+            node = node.chars[ch]
+        return node.end
+                
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
         node = self.root
-        for char in prefix:
-            if char not in node.children:
+        for ch in prefix:
+            if ch not in node.chars:
                 return False
-            node = node.children[char]
+            node = node.chars[ch]
         return True
         
 
