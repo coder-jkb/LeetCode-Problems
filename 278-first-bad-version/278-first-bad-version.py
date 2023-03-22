681648f1b58ba95fd49a971ec2420e44dd0a37c8
Jay Bhanushali <jkb17codes@gmail.com>
2023-03-22 15:59:33 +0530

    Time: 23 ms (96.87%), Space: 13.9 MB (49.16%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -5,12 +5,10 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         l = 0
         r = n-1
-        calls = 0
         while l<=r:
             mid = (l+r)//2
             # print(l,r,mid)
             bad_adj = isBadVersion(mid), isBadVersion(mid+1)
-            calls +=2
             if not bad_adj[0] and bad_adj[1]:
                 return mid+1
             
