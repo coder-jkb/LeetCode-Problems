309b5fe959f70a5b43cd397dacfd023c8eb2b91c
Jay Bhanushali <jkb17codes@gmail.com>
2023-03-30 21:20:07 +0530

    Time: 35 ms (55.28%), Space: 13.9 MB (41.82%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,7 +1,16 @@
 class Solution:
     def fib(self, n: int) -> int:
-        store = {0: 0, 1: 1}
-        if n > 1 and n not in store:
-            store[n] = self.fib(n-1) + self.fib(n-2)
-                
-        return store[n]
+        # iterative approach
+        if n in (0, 1):
+            return n
+        
+        ans = 1
+        i, j = 0, 1
+        count = 0
+        while count<n-2:
+            i = j
+            j = ans
+            ans = i + j
+            count+=1
+            
+        return ans
