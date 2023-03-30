26227df5930a4e834f58d6b6d49951895e14cdf3
Jay Bhanushali <jkb17codes@gmail.com>
2023-03-30 21:22:32 +0530

    Time: 35 ms (32.88%), Space: 13.9 MB (6.81%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,9 +1,7 @@
 class Solution:
     remaining = {1: 1, 2: 2} # for future reference
     def climbStairs(self, n: int) -> int:
-        if n in Solution.remaining:
-            return Solution.remaining[n]
-        else:
-            ways = self.climbStairs(n-1) + self.climbStairs(n-2)
-            Solution.remaining[n] = ways
-            return ways 
+        if n not in Solution.remaining:
+            Solution.remaining[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        
+        return Solution.remaining[n] 
