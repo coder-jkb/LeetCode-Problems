88a72446697c555756f1533050d50105dc522f19
Jay Bhanushali <jkb17codes@gmail.com>
2023-04-03 21:49:39 +0530

    Time: 54 ms (72.36%), Space: 13.9 MB (20.36%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -2,7 +2,9 @@ class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         max_bal = float('-inf')
         for i, bal in enumerate(accounts):
-            max_bal = max(sum(bal), max_bal)
+            sum_bal = sum(bal)
+            if sum_bal > max_bal:
+                max_bal = sum_bal
                 
         return max_bal
                 
